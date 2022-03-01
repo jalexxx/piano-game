9489aa8358b48eb4ac2abd0b59b44dcfea5f98b4
jalexxx <jumaanahalex@gmail.com>
2022-03-01 12:42:35 +0000

    inde.js for layout to export both header and the footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+// import { News } from './pages';
+import { Header, Footer } from './layout';
 import './App.css';
 
 function App() {
   return(
     <>
+      <Header />
+      {/* <News /> */}
+      <Footer />
     </>
   );
 };
---
+++ src/layout/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css'
+
+const Header = () => <nav>Header here!</nav>;
+
+export default Header;
